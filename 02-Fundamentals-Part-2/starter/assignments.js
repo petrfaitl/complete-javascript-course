598929ea74f0f23f31a2f8fe61d807857d005c2b
Petr Faitl <pfaitl@gmail.com>
2022-03-04 13:29:29 +1300

    Finished Fundamentals 2

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -58,3 +58,84 @@
 // console.log(bills);
 // console.log(tips);
 // console.log(totals);
+
+// let mark = {
+// 	firstName: "Mark",
+// 	lastName: "Miller",
+// 	mass: 78,
+// 	height: 1.68,
+// 	calcBMI: function () {
+// 		this.bmi = Number(this.mass / this.height ** 2).toFixed(2);
+// 		return this.bmi;
+// 	},
+// };
+
+// let john = {
+// 	firstName: "John",
+// 	lastName: "Smith",
+// 	mass: 30,
+// 	height: 1.55,
+// 	calcBMI: function () {
+// 		this.bmi = Number(this.mass / this.height ** 2).toFixed(2);
+// 		return this.bmi;
+// 	},
+// };
+// // const bmiMark = mark.calcBMI();
+// // const bmiJohn = john.calcBMI();
+
+// function compareBmi(person1, person2) {
+// 	console.log(person1.calcBMI());
+// 	console.log(person2.calcBMI());
+
+// 	if (person1.calcBMI() > person2.calcBMI()) {
+// 		return `${person1.firstName}'s BMI (${person1.bmi}) is higher than ${person2.firstName} (${person2.bmi}).`;
+// 	} else if (person1.bmi < person2.bmi) {
+// 		return `${person2.firstName} BMI (${person2.bmi}) is higher than ${person1.firstName} (${person1.bmi}).`;
+// 	} else {
+// 		return `${person2.firstName} BMI (${person2.bmi}) is the same as ${person1.firstName} (${person1.bmi}).`;
+// 	}
+// }
+
+// // console.log(compareBmi(mark, john));
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const calcTip = (value) => {
+	let tip;
+	if (value >= 50 && value <= 300) {
+		tip = value * 0.15;
+	} else {
+		tip = value * 0.2;
+	}
+	return tip;
+};
+
+for (const bill of bills) {
+	tips.push(calcTip(bill));
+	console.log(
+		`Total bill of $${(bill + calcTip(bill)).toFixed(2)} (tip $${calcTip(
+			bill
+		).toFixed(2)})`
+	);
+}
+
+const calcAverage = (arr) => {
+	let total = 0;
+	for (const item of arr) {
+		total += item;
+	}
+	return total / bills.length;
+};
+
+const calcAverage2 = (arr) => {
+	const avg =
+		arr.reduce((total, bill) => {
+			return (total += bill);
+		}, 0) / arr.length;
+	return avg;
+};
+
+console.log(tips);
+console.log(calcAverage(bills));
+console.log(calcAverage(tips));
+console.log(calcAverage2(bills));
+console.log(calcAverage2(tips));
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -55,6 +55,7 @@
 // console.log(fruitProcessor(2, 3));
 
 // const friends = ["Michael", "Steven", "Peter"];
+// console.log(typeof friends);
 
 // friends.push("Jay");
 
@@ -74,10 +75,151 @@
 
 // Objects
 
-const jonasArray = [
-	"jonas",
-	"Schmedtmann",
-	"35",
-	"teacher",
-	["Michael", "Peter", "Steven"],
-];
+// const jonasArray = [
+// 	"jonas",
+// 	"Schmedtmann",
+// 	"35",
+// 	"teacher",
+// 	["Michael", "Peter", "Steven"],
+// ];
+
+// const jonas = {
+// 	firstName: "Jonas",
+// 	lastName: "Schmedtmann",
+// 	birthYear: 1976,
+// 	calcAge: function () {
+// 		// console.log(jonas);
+// 		this.age = 2022 - this.birthYear;
+// 		return this.age;
+// 	},
+// 	hasDriversLicense: true,
+// 	job: "teacher",
+// 	friends: ["Michael", "Steven", "Peter"],
+// 	getSummary: function () {
+// 		this.calcAge();
+
+// 		return `${this.firstName} is a ${this.age} year old ${
+// 			this.job
+// 		}, and he ${
+// 			this.hasDriversLicense ? "has" : "does't have"
+// 		} a driver's license.`;
+// 	},
+// };
+
+// console.log(jonas);
+// console.log(jonas.age());
+// console.log(jonas.friends);
+// console.log(jonas["firstName"]);
+
+// const nameKey = "Name";
+// console.log(jonas["first" + nameKey]);
+// console.log(jonas["last" + nameKey]);
+
+// const interestedIn = prompt(
+// 	"What do you want to know about Jonas? Choose one from firstName, lastName, age, job or friends"
+// );
+
+// jonas.location = "Portugal";
+// jonas["twitter"] = "@jonasS";
+// console.log(!jonas[interestedIn] ? "Not available" : jonas[interestedIn]);
+
+// console.log(
+// 	`${jonas.firstName} has ${jonas.friends.length} friends and his best friend is called ${jonas.friends[0]}.`
+// );
+// jonas.calcAge();
+// jonas.hasDriversLicense = false;
+// console.log(jonas.getSummary());
+
+// Loops
+
+// for (let i = 0; i < 10; i++) {
+// 	console.log(`Lifting weights, repetition ${i + 1} 🏋️‍♀️`);
+// }
+// let n = 0;
+// while (n < 10) {
+// 	console.log(`Lifting weights, repetition ${n + 1} 🏋️‍♀️`);
+// 	n++;
+// }
+
+// Array Loops
+
+const addResultEle = () => {
+	const resultArea = document.querySelector("#result-area");
+	let pre = document.createElement("pre");
+	pre.id = "result";
+	pre.classList.add("col", "shadow", "py-3");
+
+	resultArea.appendChild(pre);
+};
+
+const getResultEle = () => {
+	const result = document.querySelector("#result");
+	if (result) {
+		return result;
+	} else {
+		return false;
+	}
+};
+// const calculate = () => {
+// 	let result = getResultEle();
+// 	if (!result) {
+// 		addResultEle();
+// 		result = getResultEle();
+// 	}
+// 	jonasArray.reverse();
+// 	if (result.textContent === "") {
+// 		for (const item of jonasArray) {
+// 			// console.log(item);
+// 			result.textContent += `${item} ${typeof item} \n`;
+// 		}
+// 	}
+// };
+
+// const calculate = () => {
+// 	let result = getResultEle();
+// 	if (!result) {
+// 		addResultEle();
+// 		result = getResultEle();
+// 	}
+
+// 	let dice = Math.ceil(Math.random() * 6);
+// 	while (dice !== 5) {
+// 		result.textContent += dice + "\n";
+// 		dice = Math.ceil(Math.random() * 6);
+// 		if (dice == 5) result.textContent += dice + "\n";
+// 	}
+// };
+
+const clearBtn = document.querySelector("#clear");
+const calculateBtn = document.querySelector("#calculate");
+
+clearBtn.addEventListener("click", () => {
+	const result = document.querySelector("#result");
+	if (result) {
+		result.remove();
+	}
+});
+calculateBtn.addEventListener("click", calculate);
+
+// const jonasArray = [
+// 	"Jonas",
+// 	"Schmedtmann",
+// 	35,
+// 	"teacher",
+// 	["Michael", "Peter", "Steven"],
+// ];
+
+// // const years = [1990, 1976, 1970, 1945];
+// // const ages = [];
+
+// // // for (let i = 0; i <= years.length - 1; i++) {
+// // // 	const age = 2022 - years[i];
+// // // 	ages.push(age);
+// // // }
+
+// // for (const year of years) {
+// // 	const age = 2022 - year;
+// // 	ages.push(age);
+// // }
+
+// // console.log(ages);
